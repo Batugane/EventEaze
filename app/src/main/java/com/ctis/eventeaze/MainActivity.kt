fbb03f04e2e1393b2d82b22fc53a2eb0edbd9520
BoraCelikcioglu <bora054@hotmail.com>
2023-12-26 14:34:39 +0300

    layout progress

--- app/src/main/java/com/ctis/eventeaze/MainActivity.kt
+++ app/src/main/java/com/ctis/eventeaze/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.favorite)
 
 
         eventService = ApiClient.getClient()
--- app/src/main/java/com/ctis/eventeaze/adapter/CustomRecyclerViewAdapter.kt
+++ app/src/main/java/com/ctis/eventeaze/adapter/CustomRecyclerViewAdapter.kt
@@ -1,5 +1,16 @@
 package com.ctis.eventeaze.adapter
 
-class CustomRecyclerViewAdapter {
-    //deneme
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ctis.eventeaze.R
+import com.ctis.eventeaze.db.Event
+import java.lang.reflect.Type
+
+class CustomRecyclerViewAdapter{
+
+
+
 }

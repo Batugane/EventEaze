54d13d48584943eb589b0e776187da9269ca0e47
goktu <goktugsenkoyuntr@hotmail.com>
2023-12-27 20:18:58 +0300

    FEAT:layout adjustments

--- app/src/main/java/com/ctis/eventeaze/fragment/FavoritesFragment.kt
+++ app/src/main/java/com/ctis/eventeaze/fragment/FavoritesFragment.kt
@@ -1,10 +1,14 @@
 package com.ctis.eventeaze.fragment
 
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,6 +20,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.ctis.eventeaze.R
 
 class FavoritesFragment : Fragment(),
     FavoritesRecyclerViewAdapter.FavoritesRecyclerAdapterInterface {
@@ -75,8 +80,53 @@ class FavoritesFragment : Fragment(),
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
-                val event = (binding.favoritesRecyclerView.adapter as FavoritesRecyclerViewAdapter).getEventAtPosition(position)
-                removeFavorite(event)
+                val deletedEvent = (binding.favoritesRecyclerView.adapter as FavoritesRecyclerViewAdapter).getEventAtPosition(position)
+
+                // Delete the item from your data source
+                eventViewModel.deleteEvent(deletedEvent)
+
+                // Show Snackbar with Undo option
+                Snackbar.make(binding.root, "Event deleted", Snackbar.LENGTH_LONG).setAction("Undo") {
+                    // Undo the deletion
+                    eventViewModel.addEvent(deletedEvent)
+                }.show()
+            }
+
+            override fun onChildDraw(c: Canvas, recyclerView: RecyclerView,
+                                     viewHolder: RecyclerView.ViewHolder,
+                                     dX: Float, dY: Float, actionState: Int,
+                                     isCurrentlyActive: Boolean) {
+                super.onChildDraw(
+                    c,
+                    recyclerView,
+                    viewHolder,
+                    dX,
+                    dY,
+                    actionState,
+                    isCurrentlyActive
+                )
+
+                val itemView = viewHolder.itemView
+                val background = ColorDrawable(Color.RED)
+                val icon = context?.let { ContextCompat.getDrawable(it, R.drawable.ic_delete_24) }
+                val iconMargin = (itemView.height - (icon?.intrinsicHeight ?: 0)) / 2
+                val iconTop = itemView.top + (itemView.height - (icon?.intrinsicHeight ?: 0)) / 2
+                val iconBottom = iconTop + (icon?.intrinsicHeight ?: 0)
+
+                if (dX < 0) { // Swipe to left
+                    background.setBounds(
+                        itemView.right + dX.toInt(),
+                        itemView.top,
+                        itemView.right,
+                        itemView.bottom
+                    )
+                    val iconLeft = itemView.right - iconMargin - (icon?.intrinsicWidth ?: 0)
+                    val iconRight = itemView.right - iconMargin
+                    icon?.setBounds(iconLeft, iconTop, iconRight, iconBottom)
+                }
+
+                background.draw(c)
+                icon?.draw(c)
             }
         }
 
